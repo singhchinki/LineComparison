74ea836fef9b37323d3eeb10eb94ab425bee61c1
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-17 15:03:18 +0530

    [chinki]:Checking of two Line Equality

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 LineComparision.Line linecomparision = new LineComparision.Line();
-double length = linecomparision.getLength(6, 3, 2, 8);
-Console.WriteLine("length of given Line is-" + length);
+double length1 = linecomparision.getLength(6, 3, 2, 8);
+double length2 = linecomparision.getLength(6, 3, 2, 8);
+Console.WriteLine("length of given Line is-" + length1);
+Console.WriteLine("length of given Line is-" + length2);
+Console.WriteLine(length1.Equals(length2));
