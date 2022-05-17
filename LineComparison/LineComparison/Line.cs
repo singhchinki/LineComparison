d730bffa891470505ffc14c15ca652aa05ce4577
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-17 14:42:38 +0530

    [chinki] Line length

---
+++ LineComparison/LineComparison/Line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    internal class Line
+    {
+        public double getLength(double x1, double y1, double x2, double y2)
+        {
+            double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return result;
+        }
+    }
+}
---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+LineComparision.Line linecomparision = new LineComparision.Line();
+double length = linecomparision.getLength(6, 3, 2, 8);
+Console.WriteLine("length of given Line is-" + length);
