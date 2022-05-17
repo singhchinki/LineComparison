c03dfe45c5edf4804079fa0ffa4373f8df6d233a
chinki kumari singh <singhchinkikumari@gmail.com>
2022-05-17 16:43:22 +0530

    [chinki]:Checking of Line length comprison error fix

--- LineComparison/LineComparison/Line.cs
+++ LineComparison/LineComparison/Line.cs
@@ -13,5 +13,16 @@ namespace LineComparision
             double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             return result;
         }
+        public void compare(double length1, double length2)
+        {
+            if (length1.Equals(length2))
+                Console.WriteLine("Length of line" + length1+ "is equal" + length2);
+            else if (length1 > length2)
+                Console.WriteLine("Length of line" + length1+ "is greater to" + length2);
+            else
+                Console.WriteLine("Length of line" + length1+ "is less than to" + length2);
+
+
+        }
     }
 }
--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -5,15 +5,4 @@ double length2 = linecomparision.getLength(2, 3, 2, 8);
 Console.WriteLine("length of given Line is-" + length1);
 Console.WriteLine("length of given Line is-" + length2);
 Console.WriteLine(length1.Equals(length2));
-
-private double getLength(double length1, double length2)
-{
-    if (length1.Equals(length2))
-        Console.WriteLine("Length of line" + length1, "is equal" + length2);
-    else if (length1 > length2)
-        Console.WriteLine("Length of line" + length1, "is greater to" + length2);
-    else
-        Console.WriteLine("Length of line" + length1, "is less than to" + length2);
-
-
-}
+linecomparision.compare(length1, length2);
